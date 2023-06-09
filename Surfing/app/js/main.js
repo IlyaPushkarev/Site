340319d621b8d46a17334d9b368a24dba49f35de
IlyaPushkarev <ilyapushkarev10@gmail.com>
2023-06-09 15:25:42 +0300

    changes made

--- Surfing/app/js/main.js
+++ Surfing/app/js/main.js
@@ -1,17 +1,16 @@
 $(function(){
+	$(".first-screen__slider").slick({
+		prevArrow: '<img class="slider-arrows slider-arrows_left" src="img/arrow-left.svg" alt="">',
+		nextArrow: '<img class="slider-arrows slider-arrows_right" src="img/arrow-right.svg" alt="">',
+		asNavFor: ".dots-slider",	
+	
+	})
 
-	$('.header__slider').slick({
-		infinite: true,
-		fade: true,
-		prevArrow: '<img class="slider-arrows slider-arrows_left" src="img/arrows-left.svg" alt="">',
-		nextArrow: '<img class="slider-arrows slider-arrows_right" src="img/arrows-right.svg" alt="">',
-		asNavFor: '.slider-dots'
-	});
-
-	$('.slider-dots').slick({
+	$(".dots-slider").slick({
 		slidesToShow: 4,
-		slidesToScroll: 4,
-		asNavFor: '.header__slider'
-	});
+		asNavFor: ".first-screen__slider",
+	})
 
+
+  
 });
