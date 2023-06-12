f259ada420448c62e466469238632e825328c822
IlyaPushkarev <ilyapushkarev10@gmail.com>
2023-06-12 20:11:10 +0300

    changes made

--- Surfing/app/js/main.js
+++ Surfing/app/js/main.js
@@ -2,8 +2,7 @@ $(function(){
 	$(".first-screen__slider").slick({
 		prevArrow: '<img class="slider-arrows slider-arrows_left" src="img/arrow-left.svg" alt="">',
 		nextArrow: '<img class="slider-arrows slider-arrows_right" src="img/arrow-right.svg" alt="">',
-		asNavFor: ".dots-slider",	
-	
+		asNavFor: ".dots-slider",
 	})
 
 	$(".dots-slider").slick({
@@ -11,6 +10,19 @@ $(function(){
 		asNavFor: ".first-screen__slider",
 	})
 
+	$(".surf-slider").slick({
+		slidesToShow: 4,
+		prevArrow: '<img class="slider-arrows slider-arrows_left" src="img/arrow-left.svg" alt="">',
+		nextArrow: '<img class="slider-arrows slider-arrows_right" src="img/arrow-right.svg" alt="">',
+		asNavFor: ".surf-map-slider",
+	});
 
-  
+	$(".surf-map-slider").slick({
+		slidesToShow: 8,
+		asNavFor: ".surf-slider",
+		slidesToScroll: 1,
+		arrows: false,
+		focusOnSelect: true,
+		adaptiveHeight: true,
+	})
 });
