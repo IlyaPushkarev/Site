e7a6e9cddbe09ab612c0fe92dd376705392d8fe2
IlyaPushkarev <ilyapushkarev10@gmail.com>
2023-06-14 20:46:03 +0300

    added calculator

--- Surfing/app/js/main.js
+++ Surfing/app/js/main.js
@@ -35,4 +35,50 @@ $(function(){
 		prevArrow: '<img class="slider-arrows slider-arrows_left" src="img/arrow-left.svg" alt="">',
 		nextArrow: '<img class="slider-arrows slider-arrows_right" src="img/arrow-right.svg" alt="">'
 	});
+
+	$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+	$('.quantity').each(function() {
+		var spinner = $(this),
+		input = spinner.find('input[type="number"]'),
+		btnUp = spinner.find('.quantity-up'),
+		btnDown = spinner.find('.quantity-down'),
+		min = input.attr('min'),
+		max = input.attr('max');
+
+		btnUp.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+		btnDown.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+	});
+
+	$(".quantity-button").on("click", function(){
+	const nightCount = $(".nights").val();
+	const costPerNight = $(".total").data("night");
+	const guestCount = $(".guests").val();
+	const costPerGuest = $(".total").data("guest")
+
+	let total =  nightCount * costPerNight + (guestCount - 1) * costPerGuest;
+	const currency = "USD";
+
+	$(".total").html("$" + total + " " + currency)	;
+	})
+
 });
